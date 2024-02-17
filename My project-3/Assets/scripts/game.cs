e02d202ade36f0d91219befc9471f92dcf5c7105
bluefly <hisc@swood.co.uk>
2024-02-17 23:02:28 +0000

    pog

--- My project-3/Assets/scripts/game.cs
+++ My project-3/Assets/scripts/game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,44 +7,64 @@ public class game : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    
-    public SpriteRenderer spriteRenderer;
+    public bool finished;
+    bool playerLost;
+    public SpriteRenderer sprite_Renderer;
+    public Sprite sprite;
     public Sprite[] newsprite = new Sprite[12];
     public cards cardscript;
     public GameObject CardPrefab;
+    public String[] playerhand = new String[6];
+    public String[] enemyhand = new String[6];
+    public String[] allcards = new String[12];
 
     void Start()
     {
-        
-
-        create_card("krunk", 0, 1, 1, 3, 4);
-        create_card("james", 1, 3, 3, 3, 4);
-        create_card("shapior", 2, 33, 32, 31, 4);
 
 
-       //create_card("null", 0, 0, 0, 0, 0);
+        create_card("kronk", 0, 10, 10, 10, 10);
+        create_card("politician", 1, 4, 1, 6, 2);
+        create_card("shapiro", 2, 10, 10, 10, 10);
+        create_card("lizzie", 3, 10, 10, 10, -1);
+        create_card("Trent", 4, 10, 4, 3, 10);
+        create_card("William", 5, 5, 5, 5, 5);
+        create_card("Pete", 6, 7, 2, 4, 3);
+        create_card("Andy", 7, 2, 6, 2, 4);
+        create_card("Chad", 8, 8, 3, 7, 7);
+        create_card("Tech_bro", 9, 4, 7, 3, 5);
+        create_card("Kaylor", 10, 4, 7, 9, 7);
+        create_card("league", 11, 1, 9, 1, 1);
+
+
+        //create_card("null", 0, 0, 0, 0, 0);
         //delete_card("Card");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void create_card(string name,int id, int str, int intel, int rizz, int health)
+    private void create_card(string name, int id, int str, int intel, int rizz, int health)
     {
         GameObject Circle = GameObject.Instantiate(CardPrefab);
-        Circle.transform.localPosition = new Vector3(2, 2,2);
+        Circle.transform.localPosition = new Vector3(2, 2, 2);
         Circle.name = name;
-        //Circle.Sprite = newsprite;
+        //deck[id] = name;
 
         cards cardscript = Circle.GetComponent<cards>();
-        spriteRenderer.sprite = newsprite[id];
+
+        sprite_Renderer = Circle.GetComponent<SpriteRenderer>();
+        sprite_Renderer.sprite = newsprite[id];
+
+
+
         cardscript.strength = str;
         cardscript.health = health;
         cardscript.intelligence = intel;
         cardscript.charissma = rizz;
+
     }
 
     private void delete_card(string name)
@@ -51,5 +72,30 @@ public class game : MonoBehaviour
         GameObject Circle = (GameObject.Find(name));
         Debug.Log(Circle.name);
         Destroy(Circle);
+
+    }
+
+    //subrouitne to shuffle an array of the names or ids of the cards and then
+    //give to both players
+
+    private void shuffle_deck()
+    {
+        //shuffle the deck array please
+    }
+    public void Start_Battle()
+    {
+        if (playerLost)
+        {
+
+        }
+    }
+    public void Finish_Battle()
+    {
+        finished = true;
+        Debug.Log("Hello World.");
+    }
+    public void Die(GameObject object1)
+    {
+        Destroy(object1);
     }
 }
